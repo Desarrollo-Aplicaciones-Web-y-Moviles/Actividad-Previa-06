d7e3807d16f0367915739d4a4641e8621d0876e2
aszurita <nzuritag@fiec.espol.edu.ec>
2024-10-23 23:04:39 -0500

    Ejercicio 6 Terminado y Subir documento

---
+++ C03E06/public/scripts/ejercicio.js
@@ -0,0 +1,135 @@
+// zindex como variable global
+
+var zindex = 10;
+
+let animarCartas = (carta) => {
+
+
+  carta.addEventListener("click",(e) => {
+
+        e.preventDefault();
+
+        var isShowing = false;
+
+
+        if (carta.classList.contains("show")) {
+          isShowing = true
+        }
+
+        if (carta.parentNode.classList.contains("showing")) {
+          // a card is already in view
+          document.querySelector("div.card.show").classList.remove("show");
+
+          if (isShowing) {
+            // this card was showing - reset the grid
+            document.querySelector("div.cards").classList.remove("showing");
+          } else {
+            // this card isn't showing - get in with it
+            carta.classList.add("show");
+            carta.style.zIndex = zindex;
+
+          }
+
+          zindex++;
+
+        } else {
+          // no cards in view
+          document.querySelector("div.cards").classList.add("showing");
+          carta.classList.add("show");
+          carta.style.zIndex = zindex;
+            
+
+          zindex++;
+        }
+        
+  });
+  
+    
+}
+
+/*
+    Revisar 
+        https://www.javascripttutorial.net/javascript-fetch-api/
+        https://developer.mozilla.org/es/docs/Web/API/Document/querySelectorAll
+        https://www.geeksforgeeks.org/htmlcollection-for-loop/
+        https://codetogo.io/how-to-fetch-xml-in-javascript/
+        
+*/
+
+/*Inicio*/
+
+// Petición async/await
+// Convierta la función flecha como async
+
+let cargarLibros = async () => {
+
+    let plantilla;
+
+    // Petición async/await
+    // Anteponga await a la petición fetch   
+    let resultado = await fetch("https://raw.githubusercontent.com/DAWMFIEC/DAWM-apps/datos/libros.xml")
+
+    // Anteponga await a la conversión de la variable resultado a texto
+    let data = await resultado.text()
+
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(data, "application/xml");
+  
+    let books = xml.querySelectorAll('libros > libro')
+
+    books.forEach( book => {
+
+      // Utilice el siguiente querySelector para acceder a la respuesta con la etiqueta <thumbnailUrl>
+      let thumbnailUrl = book.querySelector('thumbnailUrl')?book.querySelector('thumbnailUrl').textContent:''
+      
+      // De la respuesta, extraiga el texto dentro de las etiquetas: <title>, <isbn> y <shortDescription>.
+      // Utilice el operador condicional ternario para validar la existencia de cada etiqueta, previo a extraer el texto
+      
+      let title = book.querySelector('title')?book.querySelector('title').textContent:''
+      let isbn = book.querySelector('isbn')?book.querySelector('isbn').textContent:''
+      let shortDescription = book.querySelector('shortDescription')?book.querySelector('shortDescription').textContent:''
+      
+
+      plantilla = `
+                <div class="card">
+                  <div class="card__image-holder">
+                    <img class="card__image" src="${thumbnailUrl}" alt="${isbn}" />
+                  </div>
+                  <div class="card-title">
+                    <a href="#" class="toggle-info btn">
+                      <span class="left"></span>
+                      <span class="right"></span>
+                    </a>
+                    <h2>
+                    ${title}
+                    <small>${isbn}</small>
+                    </h2>
+                  </div>
+                  <div class="card-flap flap1">
+                    <div class="card-description">
+                      ${shortDescription}
+                    </div>
+                  </div>
+                </div>
+            `
+
+      document.getElementsByClassName('cards')[0].innerHTML += plantilla
+
+    })
+
+    document.querySelectorAll("div.card").forEach(carta => {
+        animarCartas(carta)
+    }) 
+    
+}
+
+/*Fin*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    cargarLibros()   
+})
+
+
+module.exports = {
+    cargarLibros: cargarLibros
+};
---
+++ C03E06/test/ejercicio.test.js
@@ -0,0 +1,93 @@
+const assert = require('assert');
+const chai = require('chai');
+const chaiDom = require('chai-dom');
+const requireFromString = require('require-from-string');
+const request = require('supertest');
+const app = require('../app');
+
+chai.use(chaiDom);
+
+
+describe('Test unitarios para la ruta `/`', function() {
+
+
+    it('Respuesta del servidor', function() {
+      return request(app)
+        .get('/')
+        .then((response) => {
+            assert.equal(response.status, 200)
+
+        })
+    });
+
+    let script1 = [
+      `let cargarLibros = async () => {`,
+      `let resultado = await fetch("https://raw.githubusercontent.com/DAWMFIEC/DAWM-apps/datos/libros.xml")`,
+      `let data = await resultado.text()`
+    ]
+
+    let script2 = [
+      `let title = book.querySelector('title')?book.querySelector('title').textContent:''`,
+      `let isbn = book.querySelector('isbn')?book.querySelector('isbn').textContent:''`,
+      `let shortDescription = book.querySelector('shortDescription')?book.querySelector('shortDescription').textContent:''`
+    ]
+
+    it(`En public/scripts/ejercicio.js con el código:
+
+      ${script1.join('\n')}
+      
+      `, function() {
+      return request(app)
+        .get('/scripts/ejercicio.js')
+        .then((response) => {
+
+          try {
+
+            let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+            
+            for(let elemento of script1) {
+              chai.expect(responseclean).to.contain(elemento.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,''))
+            }
+            
+          } catch (error) {
+            chai.expect.fail(`Código esperado: 
+              
+              ${script1.join('\n')}
+              
+              `);
+            return;
+          }
+        
+        })
+    });
+
+    it(`En public/scripts/ejercicio.js con el código:
+
+      ${script2.join('\n')}
+      
+      `, function() {
+      return request(app)
+        .get('/scripts/ejercicio.js')
+        .then((response) => {
+
+          try {
+
+            let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+            
+            for(let elemento of script2) {
+              chai.expect(responseclean).to.contain(elemento.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,''))
+            }
+            
+          } catch (error) {
+            chai.expect.fail(`Código esperado: 
+              
+              ${script2.join('\n')}
+              
+              `);
+            return;
+          }
+        
+        })
+    });
+
+});
